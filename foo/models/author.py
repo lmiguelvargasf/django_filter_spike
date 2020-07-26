ef3e92561ffd3245d5a148de9d6207d7970cf98e
M <m@mathsistor.com>
2020-07-25 21:09:44 -0500

    Define QuerySet and Manager for Author

--- foo/models/author.py
+++ foo/models/author.py
@@ -1,4 +1,24 @@
 from django.db import models
+from django.db.models.functions import Length
+
+
+class AuthorQuerySet(models.QuerySet):
+    def long_first_name(self):
+        return self.annotate(length=Length("first_name")).filter(length__gte=10)
+
+    def short_last_name(self):
+        return self.annotate(length=Length("last_name")).filter(length__lte=10)
+
+
+class AuthorManager(models.Manager):
+    def get_queryset(self):
+        return AuthorQuerySet(self.model, using=self._db)
+
+    def long_first_name(self):
+        return self.get_queryset().long_first_name()
+
+    def short_last_name(self):
+        return self.get_queryset().short_last_name()
 
 
 class Author(models.Model):
@@ -6,5 +26,7 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
 
+    objects = AuthorManager()
+
     def __str__(self) -> str:
-        return f'{self.first_name} {self.last_name}'
+        return f"{self.first_name} {self.last_name}"
