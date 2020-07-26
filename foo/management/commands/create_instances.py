202cc82d3cbcf9a8050fa3336f0f83b9561d8592
M <m@mathsistor.com>
2020-07-25 21:24:27 -0500

    Update command to allow creating Publisher instances

--- foo/management/commands/create_instances.py
+++ foo/management/commands/create_instances.py
@@ -1,34 +1,39 @@
+from foo.models import publisher
 from django.core.management.base import BaseCommand, CommandError
-from ...factories import DocumentFactory, BookFactory, AuthorFactory
+from ...factories import DocumentFactory, BookFactory, AuthorFactory, PublisherFactory
 
 factories = {
-    'Document': DocumentFactory,
-    'Book': BookFactory,
-    'Author': AuthorFactory
+    "Document": DocumentFactory,
+    "Book": BookFactory,
+    "Author": AuthorFactory,
+    "Publisher": PublisherFactory,
 }
 
+
 class Command(BaseCommand):
-    help = 'Creates n instances of Model'
+    help = "Creates n instances of Model"
 
     def add_arguments(self, parser):
-        parser.add_argument('n', type=int, help='number of instances')
-        parser.add_argument('model', type=str, help='model name')
+        parser.add_argument("n", type=int, help="number of instances")
+        parser.add_argument("model", type=str, help="model name")
 
     def handle(self, *args, **options):
-        n = options['n']
-        model = options['model']
+        n = options["n"]
+        model = options["model"]
 
         if n <= 0:
-            raise CommandError('n should be a positive integer')
+            raise CommandError("n should be a positive integer")
 
         try:
             factory = factories[model]
         except KeyError:
-            raise CommandError('model does not exist')
+            raise CommandError("model does not exist")
 
         for _ in range(n):
             factory()
-        
-        self.stdout.write(self.style.SUCCESS(
-            f'{n} instance{"s" if n > 1 else ""} of {model} {"were" if n > 1 else "was"} successfully created' )
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f'{n} instance{"s" if n > 1 else ""} of {model} {"were" if n > 1 else "was"} successfully created'
+            )
         )
