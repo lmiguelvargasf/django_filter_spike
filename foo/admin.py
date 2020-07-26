4a8d0c4b89de002666ffd608d591191275dbeb95
M <m@mathsistor.com>
2020-07-25 21:04:40 -0500

    Register Book and Author in Django admin

--- foo/admin.py
+++ foo/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from .models import Document
+from .models import Document, Book, Author
 
 
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
     list_display = ('name', 'size', 'file_type')
+
+admin.site.register(Book)
+admin.site.register(Author)
