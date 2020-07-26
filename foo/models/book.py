bca9bca9f3e3649834251c04836428c34a74a71d
M <m@mathsistor.com>
2020-07-26 14:55:36 -0500

    Use Author model for author in book

--- foo/models/book.py
+++ foo/models/book.py
@@ -3,8 +3,10 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        "Author", on_delete=models.CASCADE, null=True, blank=True
+    )
     pages = models.IntegerField()
 
     def __str__(self) -> str:
-        return f'{self.title} by {self.author}'
+        return f"{self.title} by {self.author}"
