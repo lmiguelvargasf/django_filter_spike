7d7ddf690773545d48e83b8aeb97350b97c36c51
M <m@mathsistor.com>
2020-07-26 11:50:28 -0500

    Remove managers from models

--- foo/models/book.py
+++ foo/models/book.py
@@ -1,35 +1,10 @@
 from django.db import models
-from django.db.models.functions import Length
-
-class BookTitleManager(models.Manager):
-    def short_titles(self):
-        return self.annotate(length=Length('title')).filter(length__lte=20)
-
-    def long_titles(self):
-        return self.annotate(length=Length('title')).filter(length__gt=20, length__lte=30)
-    
-    def very_long_titles(self):
-        return self.annotate(length=Length('title')).filter(length__gt=30)
-
-class BookPagesManager(models.Manager):
-    def small_books(self):
-        return self.filter(pages__lt=200)
-    
-    def medium_books(self):
-        return self.filter(pages__gte=200, pages__lt=300)
-    
-    def large_books(self):
-        return self.filter(pages__gte=300, pages__lte=500)
 
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     pages = models.IntegerField()
-
-    objects = models.Manager()
-    titles = BookTitleManager()
-    sizes = BookPagesManager()
 
     def __str__(self) -> str:
         return f'{self.title} by {self.author}'
--- foo/models/publisher.py
+++ foo/models/publisher.py
@@ -1,24 +1,10 @@
 from django.db import models
-from django.db.models.functions import Length
-
-
-class PublisherQuerySet(models.QuerySet):
-    def long_name(self):
-        return self.annotate(length=Length("name")).filter(length__gte=15)
-
-    def long_address(self):
-        return self.annotate(length=Length("address")).filter(length__gte=25)
-
-    def country_starts_with(self, letter):
-        return self.filter(country__startswith=letter)
 
 
 class Publisher(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=255)
     country = models.CharField(max_length=100)
-
-    objects = PublisherQuerySet.as_manager()
 
     def __str__(self) -> str:
         return self.name
