dd38cf3fba821a0f96ad94c51460da081e114a1e
M <m@mathsistor.com>
2020-07-25 19:29:00 -0500

    Implement custom manager for Document

--- foo/models.py
+++ foo/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
+class DocumentManager(models.Manager):
+    def pdfs(self):
+        return self.filter(file_type='pdf')
+
+    def smaller_than(self, size):
+        return self.filter(size__lt=size)
+
 
 class Document(models.Model):
     name = models.CharField(max_length=30)
     size = models.PositiveIntegerField(default=0)
     file_type = models.CharField(max_length=10, blank=True)
+
+    objects = DocumentManager()
 
     def __str__(self) -> str:
         return self.name
