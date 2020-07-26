2b2ecbb61e9275d8770ad687fa86275d0d6bbf6b
M <m@mathsistor.com>
2020-07-26 12:39:06 -0500

    Declare a custom field for filter

--- api/foo/filters.py
+++ api/foo/filters.py
@@ -6,11 +6,11 @@ from foo.models import Author, Book
 class AuthorFilter(filters.FilterSet):
     class Meta:
         model = Author
-        fields = ('first_name', 'last_name', 'email')
+        fields = ("first_name", "last_name", "email")
 
 
 class BookFilter(filters.FilterSet):
-
+    min_pages = filters.NumberFilter(field_name='pages', lookup_expr='gte')
     class Meta:
         model = Book
-        fields = ('title',)
+        fields = ("title",)
