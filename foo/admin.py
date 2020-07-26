658a53941eea34057a74c28a3d6a12e67a30919c
M <m@mathsistor.com>
2020-07-25 19:28:33 -0500

    Improve admin table for Document

--- foo/admin.py
+++ foo/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Document
 
-admin.site.register(Document)
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'size', 'file_type')
